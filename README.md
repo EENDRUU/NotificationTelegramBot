9c8bb2998dc365ffb4438fa623b7efbbf23ee6ff
EENDRUU <christianandrew4321@gmail.com>
2020-03-30 14:28:20 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# NotificationTelegramBot" 
---
+++ src/main/java/com/company/UserTelegram.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class User {
+    String chat_id, name;
+    int success;
+
+
+    public User(String chat_id, String name, int success) {
+        this.chat_id = chat_id;
+        this.name = name;
+        this.success = success;
+    }
+
+    public String getChat_id() {
+        return chat_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSuccess() {
+        return success;
+    }
+
+    public void setChat_id(String chat_id) {
+        this.chat_id = chat_id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSuccess(int success) {
+        this.success = success;
+    }
+}
